199f940e9b9bedb9d4266e16133aa6f4c06dbd46
lemon <l3mon@lemons-MacBook-Air.local>
2025-05-27 12:32:25 +0530

    instead of state management i used some if else to handle navbar menu(its gonna blow up)

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,25 @@
-
+import { Link, useLocation } from 'react-router-dom';
 
 function Navbar() {
+  let location = useLocation();
+
+  let token = localStorage.getItem("jwtToken")
+
   return (
     <header className="bg-[#ff0000] py-2 px-2 mb-6 text-white">
           <div className="flex items-center justify-between">
             <div className="font-bold text-lg">round3</div>
             <nav className="flex space-x-4 text-white">
-              <a href="/login" className="text-sm hover:underline text-white">
-                login
+              
+            <a href="/writers" className="text-sm hover:underline text-white">
+                writers
               </a>
+              {(location.pathname != '/login') && (location.pathname != '/post') && <a href="/login" className="text-sm hover:underline text-white">
+                login
+              </a>}
+              {token && <a href="/request" className="text-sm hover:underline">
+                post
+              </a>}
               <a href="/request" className="text-sm hover:underline">
                 invite
               </a>
