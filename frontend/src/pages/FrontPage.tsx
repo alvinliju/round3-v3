373638594274b5de9636ee53dcbc158c9d56db04
lemon <l3mon@lemons-MacBook-Air.local>
2025-05-27 14:16:54 +0530

    landing page edit

--- frontend/src/pages/FrontPage.tsx
+++ frontend/src/pages/FrontPage.tsx
@@ -13,7 +13,7 @@ function FrontPage() {
 
         <div className='flex flex-col '>
             <p className='font-bold space'>For creators:</p>
-            <p className='text-sm space-y-2'>Get paid for sharing updates about what you're already doing, get funded by your peers. <p>To create an account someone must invite you <a className='underline text-orange-500' href='/request'>send them this link.</a></p></p>
+            <p className='text-sm space-y-2'>Get paid for sharing updates about what you're already doing, get funded by your peers. <p>To create an account someone must invite you <a className='underline text-red-500' href='/request'>send them this link.</a></p></p>
 
             <p className='font-bold mt-4'>For readers:</p>
             <p className='text-sm space-y-2'>Pay 5$/m for someone cool, <br/> Get Updates. Ask Questions. Access opportunity.</p>
@@ -24,7 +24,7 @@ function FrontPage() {
         {/* Secondary CTA */}
       <div className='mt-4'>
         <p className='text-sm'>
-          Know someone who should be here? <a className='text-orange-500 underline hover:text-orange-600' href='/request'>Invite a founder</a>
+          Know someone who should be here? <a className='text-red-500 underline hover:text-red-600' href='/request'>Invite a founder</a>
         </p>
       </div>
 
