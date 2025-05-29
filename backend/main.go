4419048c843ffc55c5d4de89cebb94e32a5a788c
lemon <l3mon@lemons-MacBook-Air.local>
2025-05-29 21:18:50 +0530

    fixing cors issue fuckk

--- backend/main.go
+++ backend/main.go
@@ -131,7 +131,7 @@ func main() {
 
 	//CORS
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "https://www.round3.xyz", "https://www.bachmanfunded.xyz"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://www.round3.xyz", "https://bachmanfunded.xyz", "https://www.bachmanfunded.xyz"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
